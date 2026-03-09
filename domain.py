4b9e0fbeedbc6fb3d3a4a3b9675a462e2df11cce
Maarten Froma <maartenfroma@gmail.com>
2026-03-09 17:21:31 +0100

    Update domain.py

--- domain.py
+++ domain.py
@@ -17,17 +17,3 @@ def get_subdomain_name(url):
         return ''
 
 
-# from urllib.parse import (urlparse)
-#
-# def get_domain_name(url):
-#     try:
-#         results=get_subdomain_name(url).split('.')
-#         return results[-2] + '.' + results[-1]
-#     except:
-#         return ''
-#
-# def get_subdomain_name(url):
-#     try:
-#         return urlparse(url).netloc
-#     except:
-#         return ''
